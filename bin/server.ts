52650c933816abca77d6618548289903c71f1979
Guimarães Mahota <guimaraesmahota@gmail.com>
2020-06-23 22:49:17 +0200

    update travis

--- bin/server.ts
+++ bin/server.ts
@@ -3,7 +3,7 @@ import "https://deno.land/x/dotenv/mod.ts";
 
 const env = Deno.env.toObject()
 const PORT = Number(env.PORT || 4000)
-const HOST = env.HOST || '127.0.0.1'
+const HOST = env.API_ENDPOINT || '127.0.0.1'
 
 app.start({ hostname: HOST, port: PORT });
 
