0567ab69fa81a9d30c62dee089ea7533be82fd61
Guimarães Mahota <guimaraesmahota@gmail.com>
2020-07-05 17:02:18 +0200

    config heroku

--- bin/server.ts
+++ bin/server.ts
@@ -1,10 +1,17 @@
 import app from "../app.ts";
 import "https://deno.land/x/dotenv/mod.ts";
+import * as flags from "https://deno.land/std/flags/mod.ts";
 
-const env = Deno.env.toObject()
-const PORT = Number(env.PORT || 3000)
-const HOST = env.API_ENDPOINT || '127.0.0.1'
+const {args} = Deno;
+const DEFAULT_PORT = 8080;
+const argPort = flags.parse(args).port;
+const port = argPort ? Number(argPort) : DEFAULT_PORT;
 
-app.start({ hostname: HOST, port: PORT });
+// const env = Deno.env.toObject()
+// const PORT = Number(env.PORT || 3000)
+// const HOST = env.API_ENDPOINT || '127.0.0.1'
 
-console.log(`app running -> ${HOST}:${PORT}`);
+
+app.start({ port: port });
+
+console.log(`app running -> localhost:${PORT}`);
